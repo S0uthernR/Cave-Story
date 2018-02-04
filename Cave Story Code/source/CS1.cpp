e5858b3c42098e0d0a678e2dba600021eab6c77e
Michael Barnes <m.brandon.barnes@gmail.com>
2018-02-04 08:51:53 -0600

    Rename Cave Story Code/CS1.cpp to Cave Story Code/source/CS1.cpp

---
+++ Cave Story Code/source/CS1.cpp
@@ -0,0 +1,15 @@
+﻿// CS1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <SDL.h>
+#include "game.h"
+
+
+
+int main(int argc, char* argv[])
+{
+	Game game;
+    return 0;
+}
+
