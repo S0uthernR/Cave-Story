187343e3d0ddb852f3408454bed39cdd903b53b2
Michael Barnes <m.brandon.barnes@gmail.com>
2018-02-04 08:51:11 -0600

    Add files via upload

---
+++ Cave Story Code/header/sprite.h
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef SPRITE_H
+#define SPRITE_H
+
+#include <SDL.h>
+#include <string>
+class Graphics;
+
+
+//Sprite class holds all the information for individual sprites
+
+class Sprite {
+public:
+	Sprite();
+
+	Sprite(Graphics &graphics, const std::string &filePath, int sourceX, int sourceY, int width, int height,
+		float posX, float posY);
+	virtual ~Sprite();
+	virtual void update();
+	void draw(Graphics &graphics, int x, int y);
+
+protected:
+	SDL_Rect _sourceRect;
+	SDL_Texture* _spriteSheet;
+	float _x, _y;
+
+private:
+};
+
+
+
+
+#endif
